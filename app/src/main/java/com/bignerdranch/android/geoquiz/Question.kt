234fda6d0ef1be3884cf408c5580de887330e8b8
Justin Doak <justinhdoak@gmail.com>
2017-11-05 22:48:07 -0500

    Added Question class

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.kt
@@ -0,0 +1,6 @@
+package com.bignerdranch.android.geoquiz
+
+class Question(textResId : Int, answerTrue : Boolean) {
+    var mTextResId : Int = textResId
+    var mAnswerTrue : Boolean = answerTrue
+}
