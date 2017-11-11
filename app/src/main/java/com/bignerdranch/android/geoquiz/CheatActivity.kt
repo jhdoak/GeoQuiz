e99c80fd371c992e26e6f694d393d9287bfc045e
Justin Doak <justinhdoak@gmail.com>
2017-11-11 00:04:22 -0500

    Added new empty cheat activity

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.kt
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.geoquiz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CheatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cheat)
+    }
+}
